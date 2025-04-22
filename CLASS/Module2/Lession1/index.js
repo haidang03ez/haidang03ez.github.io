4f607519af42d13c0302a598eb93aca9adffb3d1
haidang03ez <haidangbay2003@gmail.com>
2025-04-22 21:11:57 +0700

    update

--- CLASS/Module2/Lession1/index.js
+++ CLASS/Module2/Lession1/index.js
@@ -1,3 +1,17 @@
-const a = 10;
-a = 20
-console.log(a)
+// let a = 5, b = 10, c = 15;
+
+// z = ++a + b++ + --c + a++; // 6 + 10 + 14 + 6 = 36
+// y = c-- + ++b - ++a + c++; // 14 + 12 - 8 + 13 = 31
+
+// console.log(a, b, c, z, y); //36 31 8 12 14 
+
+x = true, y = true, z = false
+
+console.log(x && y && z)
+console.log(x && y && !z)
+console.log((x && y) || z)
+console.log((x && y) || !z)
+console.log(x && (y || z))
+console.log(x && !(y || z))
+console.log(x && (y || !z))
+console.log(x && (!y || z))
