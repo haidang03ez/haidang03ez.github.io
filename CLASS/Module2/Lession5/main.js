317f07e9033920ab13994f1fea559d79264c2c72
haidang03ez <haidangbay2003@gmail.com>
2025-05-20 13:44:06 +0700

    update

---
+++ CLASS/Module2/Lession5/main.js
@@ -0,0 +1,13 @@
+
+
+let items = document.querySelectorAll(".favorite-list-item");
+items.forEach((item) => {
+  item.textContent = "I love this thing";
+  item.style.color = "red";
+});
+
+let input = prompt("Nhập món đồ yêu thích:");
+let newItem = document.createElement("li");
+newItem.className = "favorite-list-item";
+newItem.textContent = input;
+document.getElementById("favorite-list").appendChild(newItem);
