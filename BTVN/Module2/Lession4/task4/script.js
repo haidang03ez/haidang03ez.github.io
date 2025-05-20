3f500fc3e150a43dda81e2107d6c8d21d67ec4ae
haidangez <haidangbay2003@gmail.com>
2025-05-20 18:13:52 +0700

    update

--- BTVN/Module2/Lession4/task4/script.js
+++ BTVN/Module2/Lession4/task4/script.js
@@ -1,4 +1,4 @@
-// Quizzes
+// Quizzes 1
 // 1. Khai báo 1 đối tượng để mô tả bản thân bao gồm: tên, tuổi, địa chỉ, giới tính, sở thích (nhiều sở thích), chiều cao, cân nặng, tình trạng hôn nhân, số điện thoại.
 const human = {
   name: "john",
@@ -46,7 +46,7 @@ const solveQuadraticEquation = (a, b, c) => {
       : `x1 = x2 = ${-b / (2 * a)}`;
   return temp;
 };
-console.log("-----------------------------Câu 1-----------------------------");
+console.log("-----------------------------Quizz 1 - 1-----------------------------");
 console.log(solveQuadraticEquation(1, -3, 2));
 console.log(solveQuadraticEquation(0, 2, -4));
 console.log(solveQuadraticEquation(1, 2, 5));
@@ -56,7 +56,7 @@ const checkTriangle = (a, b, c) => {
   let temp = a + b > c && a + c > b && b + c > a ? "Đúng" : "Sai";
   return temp;
 };
-console.log("-----------------------------Câu 2-----------------------------");
+console.log("-----------------------------Quizz 1 - 2-----------------------------");
 console.log(checkTriangle(3, 4, 5));
 console.log(checkTriangle(1, 2, 3));
 
@@ -68,9 +68,41 @@ const checkDate = (day, month, year) => {
       : "Không hợp lệ";
   return temp;
 };
-console.log("-----------------------------Câu 3-----------------------------");
+console.log("-----------------------------Quizz 1 - 3-----------------------------");
 console.log(checkDate(31, 12, 2023));
 console.log(checkDate(31, 13, 2023));
+
+// Quizzes 2
+// 1. Khai báo 1 function nhận đầu vào là chuỗi name, thực hiện in ra màn hình “Hello world, ” + name.
+const sayHello = (name) => {
+  let temp = `Hello world, ${name}`;
+  return temp;
+};
+console.log("-----------------------------Quizz 2 - 1-----------------------------");
+console.log(sayHello("John"));
+
+// 2. Khai báo 1 function nhận đầu vào là 2 số a, b. Đầu ra là kết quả của phép tính (a + b)^2 .
+const calculateSquare = (a, b) => {
+  let temp = Math.pow(a + b, 2);
+  return temp;
+};
+console.log("-----------------------------Quizz 2 - 2-----------------------------");
+console.log(calculateSquare(2, 3));
+
+// 3. Khai báo 1 function để nhập vào a từ bàn phím, tiếp tục nhập đến khi a là một số lớn hơn 0. Đầu ra là giá trị số vừa nhập.
+const inputNumber = () => {
+  let a;
+  do {
+    a = prompt("Nhập vào số lớn hơn 0");
+    if (a <= 0) {
+      console.log("Số không hợp lệ. Vui lòng nhập lại.");
+    }
+  } while (a <= 0);
+  return a;
+}
+console.log("-----------------------------Quizz 2 - 3-----------------------------");
+console.log(inputNumber());
+
 
 // 4. Cho bộ dữ liệu về users như sau: data.js
 const { data } = require("./data.js");
